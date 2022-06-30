d012aac5aa3ab9981b6cdbe6ac90d14716e27d23
samuaaltonen <samu.aaltonen@tunturimedia.fi>
2022-07-01 01:14:42 +0300

    Added initial file and class structure.

---
+++ trustmary.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  Plugin Name: Trustmary Widgets
+  Plugin URI: https://trustmary.com/
+  Description: 
+  Version:     1.0.0
+  Author:      Samu Aaltonen
+  Author URI:
+  License:
+  License URI:
+ */
+
+defined('ABSPATH') or die('No');
+
+class Trustmary_Widgets {
+
+}
+
+new Trustmary_Widgets();
