77cee00666bab36ddee7d39d2e6a500c067a1ef2
samuaaltonen <samu.aaltonen@tunturimedia.fi>
2022-07-01 15:06:21 +0300

    Changed encryption methods to be static in helper class.

--- includes/helper.php
+++ includes/helper.php
@@ -12,14 +12,14 @@ class Trustmary_Helper
      *
      * @var string
      */
-    private $_encryption_method = 'aes-256-cbc';
+    public static $_encryption_method = 'aes-256-cbc';
 
     /**
      * Algorithm name for hash value using the HMAC method 
      *
      * @var string
      */
-    private $_hash_hmac_algo = 'sha3-512';
+    public static $_hash_hmac_algo = 'sha3-512';
 
     /**
      * Encrypts given string using SECURE_AUTH_KEY, SECURE_AUTH_SALT, 
@@ -28,13 +28,13 @@ class Trustmary_Helper
      * @param string $string
      * @return string
      */
-    public function encrypt($string)
+    public static function encrypt($string)
     {
-        $initialization_vector_length = openssl_cipher_iv_length($this->_encryption_method);
+        $initialization_vector_length = openssl_cipher_iv_length(self::$encryption_method);
         $initialization_vector = openssl_random_pseudo_bytes($initialization_vector_length);
 
-        $encrypted = openssl_encrypt($string, $this->_encryption_method, SECURE_AUTH_KEY, OPENSSL_RAW_DATA, $initialization_vector);
-        $hmac = hash_hmac($this->_hash_hmac_algo, $encrypted, SECURE_AUTH_SALT, TRUE);
+        $encrypted = openssl_encrypt($string, self::$encryption_method, SECURE_AUTH_KEY, OPENSSL_RAW_DATA, $initialization_vector);
+        $hmac = hash_hmac(self::$hash_hmac_algo, $encrypted, SECURE_AUTH_SALT, TRUE);
 
         $output = base64_encode($initialization_vector . $hmac . $encrypted);
         return $output;
@@ -46,18 +46,18 @@ class Trustmary_Helper
      * @param string $encrypted
      * @return string
      */
-    public function decrypt($encrypted)
+    public static function decrypt($encrypted)
     {
         $raw = base64_decode($encrypted);
 
-        $initialization_vector_length = openssl_cipher_iv_length($this->_encryption_method);
+        $initialization_vector_length = openssl_cipher_iv_length(self::$encryption_method);
         $initialization_vector = substr($raw, 0, $initialization_vector_length);
 
         $hmac_part = substr($raw, $initialization_vector_length, 64);
         $data_part = substr($raw, $initialization_vector_length + 64);
 
-        $data = openssl_decrypt($data_part, $this->_encryption_method, SECURE_AUTH_KEY, OPENSSL_RAW_DATA, $initialization_vector);
-        $hmac = hash_hmac($this->_hash_hmac_algo, $data_part, SECURE_AUTH_SALT, TRUE);
+        $data = openssl_decrypt($data_part, self::$encryption_method, SECURE_AUTH_KEY, OPENSSL_RAW_DATA, $initialization_vector);
+        $hmac = hash_hmac(self::$hash_hmac_algo, $data_part, SECURE_AUTH_SALT, TRUE);
 
         if (hash_equals($hmac_part, $hmac))
             return $data;
